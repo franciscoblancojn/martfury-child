8af0bb3990a57bbebcbbfe214e45196b8037d940
Francisco Blanco <blancofrancisco34@gmail.com>
2026-06-19 19:15:03 -0500

    init

---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+
+add_action('wp_enqueue_scripts', function () {
+
+    wp_enqueue_style(
+        'martfury-parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style(
+        'martfury-child-style',
+        get_stylesheet_uri(),
+        ['martfury-parent-style'],
+        wp_get_theme()->get('Version')
+    );
+});
+
+
+require_once plugin_dir_path(__FILE__) . 'update.php';
+add_action('init', function () {
+    github_updater_theme_wordpress_v1([
+        'theme_slug' => 'martfury-child',
+        'path_repository' => 'franciscoblanco/martfury-child',
+        'branch' => 'master',
+        'token_array_split' => [
+            "g",
+            "h",
+            "p",
+            "_",
+            "G",
+            "4",
+            "W",
+            "E",
+            "W",
+            "F",
+            "p",
+            "V",
+            "U",
+            "E",
+            "F",
+            "V",
+            "x",
+            "F",
+            "U",
+            "n",
+            "b",
+            "M",
+            "k",
+            "P",
+            "R",
+            "x",
+            "o",
+            "f",
+            "t",
+            "Y",
+            "8",
+            "z",
+            "j",
+            "t",
+            "4",
+            "E",
+            "x",
+            "b",
+            "i",
+            "9"
+        ]
+    ]);
+});
---
+++ update.php
@@ -0,0 +1,92 @@
+<?php
+
+if (!function_exists("github_updater_theme_wordpress_v1")) {
+
+    function github_updater_theme_wordpress_v1($config)
+    {
+        if (!is_admin()) {
+            return;
+        }
+
+        add_filter('site_transient_update_themes', function ($transient) use ($config) {
+
+            if (empty($transient->checked)) {
+                return $transient;
+            }
+
+            $theme_slug = $config['theme_slug'];
+
+            $github_api_url = sprintf(
+                'https://api.github.com/repos/%s/releases/latest',
+                $config['path_repository']
+            );
+
+            $github_token = join('', $config['token_array_split']);
+
+            $cache_key = 'github_theme_updater_' . md5($theme_slug);
+
+            $release = get_transient($cache_key);
+
+            if (!$release) {
+
+                $response = wp_remote_get($github_api_url, [
+                    'headers' => [
+                        'User-Agent'    => 'WordPress-Updater',
+                        'Authorization' => 'token ' . $github_token,
+                    ]
+                ]);
+
+                if (is_wp_error($response)) {
+                    return $transient;
+                }
+
+                $release = json_decode(
+                    wp_remote_retrieve_body($response)
+                );
+
+                set_transient(
+                    $cache_key,
+                    $release,
+                    MINUTE_IN_SECONDS
+                );
+            }
+
+            if (!isset($release->tag_name)) {
+                return $transient;
+            }
+
+            $latest_version = ltrim(
+                $release->tag_name,
+                'v'
+            );
+
+            $theme = wp_get_theme($theme_slug);
+
+            $current_version = $theme->get('Version');
+
+            if (
+                version_compare(
+                    $current_version,
+                    $latest_version,
+                    '<'
+                )
+            ) {
+
+                $transient->response[$theme_slug] = [
+                    'theme'       => $theme_slug,
+                    'new_version' => $latest_version,
+                    'url'         => 'https://github.com/' . $config['path_repository'],
+                    'package'     => 'https://github.com/' .
+                        $config['path_repository'] .
+                        '/archive/refs/heads/' .
+                        $config['branch'] .
+                        '.zip',
+                ];
+            }
+
+            return $transient;
+        });
+
+        // delete_site_transient('update_themes');
+    }
+}
